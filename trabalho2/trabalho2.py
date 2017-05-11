ccfa373f18bb4d93818ac1e88b1229d980d39839
FranciscoRafael <francisco.carneiro14@uol.com.br>
2017-05-11 00:46:33 -0300

    plus

--- trabalho2/trabalho2.py
+++ trabalho2/trabalho2.py
@@ -13,44 +13,65 @@ import numpy as np
 import matplotlib.pyplot as plt
 import math
 import cv2
+import skvideo.io
 
 
 def read_video(nome):
-	cap = cv2.VideoCapture(nome)
-	i = 0;
-
-	num_frame = int(cap.get(cv2.cv.CV_CAP_PROP_FRAME_COUNT))
-	num_width = int(cap.get(cv2.cv.CV_CAP_PROP_FRAME_WIDTH))
-	num_height = int(cap.get(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT))
-
-	buf = np.empty((num_frame, num_height, num_width, 3), np.dtype('uint8'))
-
-	i = 0
-	ret = True
-
-	while (i < num_frame  and ret):
-	    ret, buf[i] = cap.read()
-	    i += 1
-
-	cap.release()
-	return buf
+	videodata = skvideo.io.vread(nome)
+	return videodata
 
 def convert_gray(video):
 	frames = video.shape[0]
 	x = video.shape[1]
 	y = video.shape[2]
 	new_video = np.empty((frames, x, y))
-	print(new_video.shape)
 	for i in range((video.shape[0])):
-		arr = np.asarray(video[i])
-		plt.imshow(arr)
-		plt.show()
 		new_video[i] = cv2.cvtColor(video[i], cv2.COLOR_BGR2GRAY)
-		arr = np.asarray(new_video[i])
-		plt.imshow(arr, cmap='gray')
-		plt.show()
-
 	return new_video
+
+
+def pixels_t1(video):
+	frames = video.shape[0]
+	vect = np.zeros(frames - 1)
+	for k in range (frames - 1):
+		vect[k] = np.count_nonzero((abs(video[k] - video[k + 1]) > 115))
+
+	return vect 
+
+
+def square_sum_diff(video, div):
+	frames = video.shape[0]
+	m = video.shape[1]
+	n = video.shape[2]
+	xbloco = m/div
+	ybloco = n/div
+	count = 0
+	vect = np.zeros(frames -1)
+	for k in range(frames -1):
+		for i in range(div):
+			for j in range (div):
+				x = video[k][i*xbloco:(i+1)*xbloco, j*ybloco:(j+1)*ybloco]
+				y = video[k+1][i*xbloco:(i+1)*xbloco, j*ybloco:(j+1)*ybloco]
+				B = np.sum((x-y)**2)
+				if(B > 150000):
+					count = count + 1
+		vect[k] = count
+		count = 0
+	
+
+	return vect
+
+
+
 
 video = read_video("umn.mp4")
 video = convert_gray(video)
+print(video.shape)
+dif = square_sum_diff(video, 16)
+#dif = pixels_t1(video)
+
+t = np.arange(len(dif))
+s = dif
+plt.plot(t, s)
+plt.show()
+
