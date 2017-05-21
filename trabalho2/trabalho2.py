3b45125404c00149cbc9c8f17dd06a1cde09a3b9
FranciscoRafael <francisco.carneiro14@uol.com.br>
2017-05-21 04:47:10 -0300

    o.o

--- trabalho2/trabalho2.py
+++ trabalho2/trabalho2.py
@@ -4,9 +4,7 @@
 # Trabalho 2 - MC920 - Introducao ao Processamento e Analise de Imagens
 #
 #
-#
-#
-#
+
 from scipy import misc
 from scipy import ndimage
 import numpy as np
@@ -14,6 +12,10 @@ import matplotlib.pyplot as plt
 import math
 import cv2
 import skvideo.io
+from skimage import feature
+from skimage.filters import roberts, sobel, scharr, prewitt
+
+
 
 
 def read_video(nome):
@@ -34,7 +36,7 @@ def pixels_t1(video):
 	frames = video.shape[0]
 	vect = np.zeros(frames - 1)
 	for k in range (frames - 1):
-		vect[k] = np.count_nonzero((abs(video[k] - video[k + 1]) > 115))
+	   vect[k] = np.count_nonzero((abs(video[k] - video[k + 1]) > 128))
 
 	return vect 
 
@@ -61,14 +63,66 @@ def square_sum_diff(video, div):
 
 	return vect
 
+def edge_det(video):
+
+    frames = video.shape[0]
+    vect = np.zeros(frames-1)
+    for k in range(frames -1):
+        edge = feature.canny(video[k], sigma = 0)
+        edge1 = feature.canny(video[k+1], sigma = 0)
+        x = np.count_nonzero(edge != edge1)
+        vect[k] = x
+
+    return vect
+
+
+def find_t(vect, T):
+    tam = len(vect)
+    new = np.zeros(tam)
+
+    for i in range (tam):
+        if vect[i] > T:
+            if (i >= 0 or i< (tam-1) and vect[i-1] < 0.3*vect[i]):
+                new[i] = i
+
+    k = np.count_nonzero(new)
+    adr = np.zeros(k)
+    j = 0; 
+    for i in range(tam):
+        if(new[i] != 0):
+            adr[j] = new[i]
+            j = j+1   
+
+    return adr
+
+
+def video_frames (video, adr):
+    frames = video.shape[0]
+    m = video.shape[1]
+    n = video.shape[2]
+    tam = len(adr);
+    final = np.zeros((tam, m, n, 3));
+    for k in range (tam):
+        t = int(adr[k])
+        print t
+        final[k] = np.copy(video[t])
+
+    return final      
+
+
+video = read_video("toy.mp4")
+videog = convert_gray(video)
+
+
+dif = pixels_t1(videog)
+
+j = find_t(dif, 10)
+x = video_frames(video, j)
+print x.shape[0]
 
 
 
-video = read_video("umn.mp4")
-video = convert_gray(video)
-print(video.shape)
-dif = square_sum_diff(video, 16)
-#dif = pixels_t1(video)
+skvideo.io.vwrite("outputvideo.mp4", x)
 
 t = np.arange(len(dif))
 s = dif
